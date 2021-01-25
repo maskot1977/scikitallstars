84b3ee235abdc40b5865a327faddf0e02f979d04
Ikemen Mas Kot <maskot1977@gmail.com>
2021-01-25 13:32:04 +0900

    Update all_sklearn.py

--- allpossible/all_sklearn.py
+++ allpossible/all_sklearn.py
@@ -289,3 +289,39 @@ class NullScaler(BaseEstimator, TransformerMixin):
 
     def transform(self, x, y=None):
         return x
+
+def objective_summary(objective):
+    fig, axes = plt.subplots(nrows=1, ncols=4, figsize=(16, 8))
+
+    names = [n for n in reversed(sorted(list(objective.scores.keys())))]
+
+    score_means = []
+    score_stds = []
+    second_means = []
+    second_stds = []
+    selected = []
+    sum_second = []
+    for name in names:
+        score_means.append(np.array(objective.scores[name]).mean())
+        score_stds.append(np.array(objective.scores[name]).std())
+        second_means.append(np.array(objective.times[name]).mean())
+        second_stds.append(np.array(objective.times[name]).std())
+        selected.append(len(objective.times[name]))
+        sum_second.append(sum(objective.times[name]))
+
+    axes[0].barh(names, score_means, xerr=score_stds)
+    axes[0].set_xlabel('score')
+    axes[0].grid()
+    axes[1].barh(names, selected)
+    axes[1].set_xlabel('selected (times)')
+    axes[1].grid()
+    axes[1].yaxis.set_visible(False)
+    axes[2].barh(names, second_means, xerr=second_stds)
+    axes[2].set_xlabel('calculation time (seconds)')
+    axes[2].grid()
+    axes[2].yaxis.set_visible(False)
+    axes[3].barh(names, sum_second)
+    axes[3].set_xlabel('sum calculation time (seconds)')
+    axes[3].grid()
+    axes[3].yaxis.set_visible(False)
+    plt.show()
