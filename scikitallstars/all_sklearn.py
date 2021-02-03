cfcd7fa1089989f2ffd9deaa91a73ad7198b4892
Ikemen Mas Kot <maskot1977@gmail.com>
2021-02-03 17:38:14 +0900

    Update all_sklearn.py

--- scikitallstars/all_sklearn.py
+++ scikitallstars/all_sklearn.py
@@ -371,7 +371,7 @@ class NullScaler(BaseEstimator, TransformerMixin):
 def objective_summary(objective):
     fig, axes = plt.subplots(nrows=1, ncols=4, figsize=(16, 8))
 
-    names = [n for n in reversed(sorted(list(objective.scores.keys())))]
+    names = [n for n in reversed(list(objective.scores.keys()))]
 
     score_means = []
     score_stds = []
