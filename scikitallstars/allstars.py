8dbdccdb77e0ab40193e0870ce78f5728ca43f79
Ikemen Mas Kot <maskot1977@gmail.com>
2021-02-24 21:29:08 +0900

    Update allstars.py

--- scikitallstars/allstars.py
+++ scikitallstars/allstars.py
@@ -1201,3 +1201,17 @@ def remove_low_variance_features(df, threshold=0.0):
             pass
     
     return df.iloc[:, ok_id]
+
+def remove_high_correlation_features(df, threshold=0.95):
+    corrcoef = np.corrcoef(df.T.values.tolist())
+    selected_or_not = {}
+    for i, array in enumerate(corrcoef):
+        if i not in selected_or_not.keys():
+            selected_or_not[i] = True
+        if selected_or_not[i]:
+            for j, ary in enumerate(array): 
+                if i < j:
+                    if abs(ary) >= threshold:
+                        selected_or_not[j] = False
+
+    return df.iloc[:, [i for i, array in enumerate(corrcoef) if selected_or_not[i]]]
