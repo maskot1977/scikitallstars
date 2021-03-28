f52e57dde26009c1ba351045020b2e5b881a77de
Ikemen Mas Kot <maskot1977@gmail.com>
2021-03-29 01:27:04 +0900

    Update depict.py

--- scikitallstars/depict.py
+++ scikitallstars/depict.py
@@ -84,8 +84,12 @@ def metrics(model, X_train, y_train, X_test = None, y_test = None):
         X_test = pd.DataFrame(X_test)
     if y_test is not None:
         y_test = pd.DataFrame(y_test)
-    if !model.is_regressor:
-    #if hasattr(model, "predict_proba") or hasattr(model, "decision_function"):
+    if hasattr(model, "is_regressor"):
+        if model.is_regressor:
+            regression_metrics(model, X_train, y_train, X_test, y_test)
+        else:
+            classification_metrics(model, X_train, y_train, X_test, y_test)
+    elif hasattr(model, "predict_proba") or hasattr(model, "decision_function"):
         classification_metrics(model, X_train, y_train, X_test, y_test)
     else:
         regression_metrics(model, X_train, y_train, X_test, y_test)
