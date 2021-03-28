3900eefab4a4862ade3d59376eb4b5534af9384f
Ikemen Mas Kot <maskot1977@gmail.com>
2021-03-28 21:42:47 +0900

    Update depict.py

--- scikitallstars/depict.py
+++ scikitallstars/depict.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
 
 def best_scores(allstars_model):
     keys = list(allstars_model.best_scores.keys())
