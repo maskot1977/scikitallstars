fc3c373ad84c2f5de189323ecdd02e10ace2b157
Ikemen Mas Kot <maskot1977@gmail.com>
2021-04-05 11:46:39 +0900

    Update allstars.py

--- scikitallstars/allstars.py
+++ scikitallstars/allstars.py
@@ -851,9 +851,10 @@ def random_forest_feature_selector(X_train, y_train, timeout=30, n_trials=20, sh
 
 
 
-def fit(X_train, y_train, feature_selection=True, verbose=True, timeout=100, n_trials=100, show_progress_bar=True):
+def fit(X_train, y_train, feature_selection=True, verbose=True, timeout=100, n_trials=100, show_progress_bar=True): 
     X_train = pd.DataFrame(X_train)
-    y_train = pd.DataFrame(y_train)[0]
+    if type(y_train) is not pd.core.series.Series:
+        y_train = pd.DataFrame(y_train)[0]
     if feature_selection:
         support = random_forest_feature_selector(X_train, y_train)
         X_train_selected = X_train.iloc[:, support]
