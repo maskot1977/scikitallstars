db7513e31bb228479f63700906d0125d997b97a3
Masaaki Kotera <kotera@preferred.jp>
2021-04-30 12:05:39 +0900

    test added

--- tests/tests.py
+++ tests/tests.py
@@ -2,8 +2,18 @@
 import pytest
 import pandas as pd
 from scikitallstars import allstars, depict
+import sklearn.datasets
+from sklearn.model_selection import train_test_split
 
-def test_allstars_objective():
-    x_train = pd.DataFrame([])
-    y_train = pd.DataFrame([])
-    objective = allstars.Objective(x_train, y_train)
+
+def test_allstars_classification():
+    dataset = sklearn.datasets.load_breast_cancer()
+    X_train, X_test, y_train, y_test = train_test_split(dataset.data, dataset.target, test_size=.4) 
+    allstars_model = allstars.fit(X_train, y_train, timeout=100, n_trials=10, feature_selection=False)
+
+
+def test_allstars_regression():
+    dataset = sklearn.datasets.load_diabetes()
+    X_train, X_test, y_train, y_test = train_test_split(dataset.data, dataset.target, test_size=.4) 
+    allstars_model = allstars.fit(X_train, y_train, timeout=100, n_trials=10, feature_selection=False)
+
