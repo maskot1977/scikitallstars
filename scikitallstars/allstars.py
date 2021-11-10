14fe698e9dcf740f48250804293d6820b64b743e
Ikemen Mas Kot <maskot1977@gmail.com>
2021-11-10 13:30:51 +0900

    Update allstars.py

--- scikitallstars/allstars.py
+++ scikitallstars/allstars.py
@@ -658,15 +658,7 @@ class Objective:
 
 
 
-class NullScaler(BaseEstimator, TransformerMixin):
-    def __init__(self):
-        pass
 
-    def fit(self, x, y=None):
-        return self
-
-    def transform(self, x, y=None):
-        return x
 
 
 def random_forest_feature_selector(
