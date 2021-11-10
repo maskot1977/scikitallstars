54591bf40d1ad1b7757367535ff64d359fc4d4ca
Ikemen Mas Kot <maskot1977@gmail.com>
2021-11-10 15:37:12 +0900

    Update stacking.py

--- scikitallstars/stacking.py
+++ scikitallstars/stacking.py
@@ -68,7 +68,7 @@ class StackingObjective:
             return 0 - 530000
 
         if True:  # self.support is None:
-            x_train, x_test, y_train, y_test = kmeans_split(
+            x_train, x_test, y_train, y_test = train_test_split(
                 self.x_train, self.y_train, test_size=self.test_size, random_state=self.train_random_state
             )
         else:
