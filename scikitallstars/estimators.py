6789c2cad249e816e93f3e17008cd919fa573bbe
Ikemen Mas Kot <maskot1977@gmail.com>
2022-03-23 14:27:45 +0900

    Update estimators.py

--- scikitallstars/estimators.py
+++ scikitallstars/estimators.py
@@ -116,7 +116,7 @@ class Classifier:
 
         return None
 
-    @on_timeout(limit=600, handler=handler_func, hint=u"classifier.fit")
+    @on_timeout(limit=60, handler=handler_func, hint=u"classifier.fit")
     def fit(self, x, y, support=None):
         self._fit_and_predict_core(x, y, fitting=True, support=support)
         return self
@@ -213,7 +213,7 @@ class Regressor:
 
         return None
 
-    @on_timeout(limit=600, handler=handler_func, hint=u"regressor.fit")
+    @on_timeout(limit=60, handler=handler_func, hint=u"regressor.fit")
     def fit(self, x, y, support=None):
         self._fit_and_predict_core(x, y, fitting=True, support=support)
         return self
